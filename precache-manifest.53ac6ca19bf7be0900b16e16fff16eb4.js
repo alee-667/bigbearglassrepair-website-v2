9539054c01806534d3a1d99df0d0d9253b83a37b
owoEX <respondalex@gmail.com>
2022-09-25 21:23:39 -0700

    Updates

---
+++ precache-manifest.53ac6ca19bf7be0900b16e16fff16eb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557b12dcc81e1bffa1fba505453c7388",
+    "url": "/bigbearglassrepair-website-v2/index.html"
+  },
+  {
+    "revision": "a5a7267540d20c27627a",
+    "url": "/bigbearglassrepair-website-v2/static/css/main.f66bc0e9.chunk.css"
+  },
+  {
+    "revision": "af906cc3e031712135e4",
+    "url": "/bigbearglassrepair-website-v2/static/js/2.b1bc76b1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/bigbearglassrepair-website-v2/static/js/2.b1bc76b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a7267540d20c27627a",
+    "url": "/bigbearglassrepair-website-v2/static/js/main.6551c880.chunk.js"
+  },
+  {
+    "revision": "6704a05d73d95e0b9abc",
+    "url": "/bigbearglassrepair-website-v2/static/js/runtime-main.c81c3364.js"
+  }
+]);
