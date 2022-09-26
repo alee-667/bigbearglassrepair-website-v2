62330d4100934b2c64b5e9264f427cf4523e45b2
owoEX <respondalex@gmail.com>
2022-09-25 22:19:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
@@ -16,10 +16,10 @@ function App() {
         <Navbar />
         <ScrollToTop/>
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path='/bigbearglassrepair-website-v2' exact component={Home} />
+          <Route path='/bigbearglassrepair-website-v2/services' component={Services} />
+          <Route path='/bigbearglassrepair-website-v2/products' component={Products} />
+          <Route path='/bigbearglassrepair-website-v2/sign-up' component={SignUp} />
         </Switch>
       </Router>
     </>
